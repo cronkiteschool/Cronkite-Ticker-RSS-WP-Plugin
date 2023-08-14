0430e101532fda53268b66678fe31e52f2e0e13a
Jeremy Leggat <jleggat@asu.edu>
2023-08-14 12:48:53 -0700

    Initial commit

---
+++ Cronkite-Ticker-RSS.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Cronkite Ticker RSS
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     Cronkite-Ticker-RSS
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cronkite_Ticker_RSS
+ */
+
+// Your code starts here.
