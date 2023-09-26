80e5e50688c2d64b368dc6f3dd45de654e35cac8
Jeremy Leggat <jleggat@asu.edu>
2023-09-25 18:46:56 -0700

    Format code to PHP standards
    run php-cs-fixer on code to fix formatting

--- Cronkite-Ticker-RSS.php
+++ Cronkite-Ticker-RSS.php
@@ -19,128 +19,138 @@ class CronkiteTicker
 {
     private $pluginName = "csjticker";
 
-	private function __construct() {
-	    // Hook into the admin menu
-	    add_action( 'admin_menu', array( $this, 'create_plugin_settings_page' ) );
-	    add_action( 'admin_init', array( $this, 'setup_sections' ) );
-	    add_action( 'admin_init', array( $this, 'setup_fields' ) );
-	    add_action( 'init', array( $this, 'setup_rss' ) );
-	}
-
-    private function create_plugin_settings_page() {
-	    // Add the menu item and page
-	    $page_title = 'Ticker Feed Settings Page';
-	    $menu_title = 'Ticker Feed Plugin';
-	    $capability = 'manage_options';
-	    $slug = 'csjticker_fields';
-	    $callback = array( $this, 'plugin_settings_page_content' );
-
-	    add_submenu_page( 'options-general.php', $page_title, $menu_title, $capability, $slug, $callback, );
+    private function __construct()
+    {
+        // Hook into the admin menu
+        add_action('admin_menu', array( $this, 'create_plugin_settings_page' ));
+        add_action('admin_init', array( $this, 'setup_sections' ));
+        add_action('admin_init', array( $this, 'setup_fields' ));
+        add_action('init', array( $this, 'setup_rss' ));
     }
 
-	private function plugin_settings_page_content() { ?>
+    private function create_plugin_settings_page()
+    {
+        // Add the menu item and page
+        $page_title = 'Ticker Feed Settings Page';
+        $menu_title = 'Ticker Feed Plugin';
+        $capability = 'manage_options';
+        $slug = 'csjticker_fields';
+        $callback = array( $this, 'plugin_settings_page_content' );
+
+        add_submenu_page('options-general.php', $page_title, $menu_title, $capability, $slug, $callback, );
+    }
+
+    private function plugin_settings_page_content()
+    { ?>
 	    <div class="wrap">
 		<h2>Cronkite Ticker Settings Page</h2>
 		<form method="post" action="options.php">
 		    <?php
-			settings_fields( 'csjticker_fields' );
-			do_settings_sections( 'csjticker_fields' );
-			submit_button();
-		    ?>
+            settings_fields('csjticker_fields');
+        do_settings_sections('csjticker_fields');
+        submit_button();
+        ?>
 		</form>
 	    </div> <?php
-	}
+    }
 
-	private function setup_sections() {
-	    add_settings_section( 'config_section', 'Configuration', array( $this, 'section_callback' ), 'csjticker_fields' );
-	}
+    private function setup_sections()
+    {
+        add_settings_section('config_section', 'Configuration', array( $this, 'section_callback' ), 'csjticker_fields');
+    }
 
-	private function section_callback( $arguments ) {
-	    switch( $arguments['id'] ){
-		case 'config_section':
-		    echo 'Set to read text from a Google Doc to RSS feed for the building\'s Ticker';
-		    break;
-		case 'test_section':
-		    echo 'Test reading text from a Google Doc to RSS feed';
-		    break;
-	    }
-	}
+    private function section_callback($arguments)
+    {
+        switch($arguments['id']) {
+            case 'config_section':
+                echo 'Set to read text from a Google Doc to RSS feed for the building\'s Ticker';
+                break;
+            case 'test_section':
+                echo 'Test reading text from a Google Doc to RSS feed';
+                break;
+        }
+    }
 
-	private function setup_fields() {
-	    $fields = array(
-		array(
-		    'uid' => 'feed_name',
-		    'label' => 'Feed Name',
-		    'section' => 'config_section',
-		    'type' => 'text',
-		    'options' => false,
-		    'placeholder' => 'feedname',
-		    'helper' => 'Keep this name simple as it is used to forms your this feed URL.',
-		    'supplemental' => sprintf("The feed will be available at %s<em>%s</em>", site_url('/feed/'), get_option('feed_name')),
-		    'default' => 'ticker'
-		),
-		array(
-		    'uid' => 'gdoc_id',
-		    'label' => 'Google Doc File ID',
-		    'section' => 'config_section',
-		    'type' => 'text',
-		    'options' => false,
-		    'placeholder' => 'documentId',
-		    'helper' => 'This ID is the value between the "/d/" and the "/edit" in the URL of your document.',
-		    'supplemental' => 'The document in your Google Drive has a URL like: https://docs.google.com/document/d/<em>documentId</em>/edit',
-		    'default' => ''
-		)
-	    );
-	    foreach( $fields as $field ){
-		add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), 'csjticker_fields', $field['section'], $field );
-		register_setting( 'csjticker_fields', $field['uid'] );
-	    }
-	}
+    private function setup_fields()
+    {
+        $fields = array(
+        array(
+            'uid' => 'feed_name',
+            'label' => 'Feed Name',
+            'section' => 'config_section',
+            'type' => 'text',
+            'options' => false,
+            'placeholder' => 'feedname',
+            'helper' => 'Keep this name simple as it is used to forms your this feed URL.',
+            'supplemental' => sprintf("The feed will be available at %s<em>%s</em>", site_url('/feed/'), get_option('feed_name')),
+            'default' => 'ticker'
+        ),
+        array(
+            'uid' => 'gdoc_id',
+            'label' => 'Google Doc File ID',
+            'section' => 'config_section',
+            'type' => 'text',
+            'options' => false,
+            'placeholder' => 'documentId',
+            'helper' => 'This ID is the value between the "/d/" and the "/edit" in the URL of your document.',
+            'supplemental' => 'The document in your Google Drive has a URL like: https://docs.google.com/document/d/<em>documentId</em>/edit',
+            'default' => ''
+        )
+        );
+        foreach($fields as $field) {
+            add_settings_field($field['uid'], $field['label'], array( $this, 'field_callback' ), 'csjticker_fields', $field['section'], $field);
+            register_setting('csjticker_fields', $field['uid']);
+        }
+    }
 
-	private function field_callback( $arguments ) {
-	    $value = get_option( $arguments['uid'] ); // Get the current value, if there is one
-	    if( ! $value ) { // If no value exists
-		$value = $arguments['default']; // Set to our default
-	    }
+    private function field_callback($arguments)
+    {
+        $value = get_option($arguments['uid']); // Get the current value, if there is one
+        if(! $value) { // If no value exists
+            $value = $arguments['default']; // Set to our default
+        }
 
-	    // Check which type of field we want
-	    switch( $arguments['type'] ){
-		case 'text': // If it is a text field
-		    printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
-		    break;
-		case 'button': // If it is a button
-		    printf( '<input name="%1$s" id="%1$s" type="%2$s" value="%3$s" />', $arguments['uid'], $arguments['type'], $value );
-		    break;
-	    }
+        // Check which type of field we want
+        switch($arguments['type']) {
+            case 'text': // If it is a text field
+                printf('<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value);
+                break;
+            case 'button': // If it is a button
+                printf('<input name="%1$s" id="%1$s" type="%2$s" value="%3$s" />', $arguments['uid'], $arguments['type'], $value);
+                break;
+        }
 
-	    // If there is help text
-	    if( $helper = $arguments['helper'] ){
-		printf( '<span class="helper"> %s</span>', $helper ); // Show it
-	    }
+        // If there is help text
+        if($helper = $arguments['helper']) {
+            printf('<span class="helper"> %s</span>', $helper); // Show it
+        }
 
-	    // If there is supplemental text
-	    if( $supplimental = $arguments['supplemental'] ){
-		printf( '<p class="description">%s</p>', $supplimental ); // Show it
-	    }
-	}
+        // If there is supplemental text
+        if($supplimental = $arguments['supplemental']) {
+            printf('<p class="description">%s</p>', $supplimental); // Show it
+        }
+    }
 
-	private function setup_rss() {
-	    add_feed( get_option('feed_name'), array( $this, 'rss_callback' ) );
-	}
+    private function setup_rss()
+    {
+        add_feed(get_option('feed_name'), array( $this, 'rss_callback' ));
+    }
 
-	private function rss_callback() {
-		$body     = $this->fetch_gdoc_text();
-	    include_once( plugin_dir_path( __FILE__ ) . 'rss-cronkite-ticker.php' );
-	}
+    private function rss_callback()
+    {
+        $body     = $this->fetch_gdoc_text();
+        include_once(plugin_dir_path(__FILE__) . 'rss-cronkite-ticker.php');
+    }
 
-	private function fetch_gdoc_text() {
-		$url = sprintf("https://docs.google.com/document/d/%s/export?format=txt", get_option('gdoc_id'));
-		$response = wp_safe_remote_get( $url );
-		$body     = wp_remote_retrieve_body( $response );
-		$body     = trim($body, "\xEF\xBB\xBF"); //remove hidden utf characters
+    private function fetch_gdoc_text()
+    {
+        $url = sprintf("https://docs.google.com/document/d/%s/export?format=txt", get_option('gdoc_id'));
+        $response = wp_safe_remote_get($url);
+        $body     = wp_remote_retrieve_body($response);
+        $body     = trim($body, "\xEF\xBB\xBF"); //remove hidden utf characters
 
-		return sanitize_textarea_field( $body );
-	}
+        return sanitize_textarea_field($body);
+    }
 
 }
 
--- rss-cronkite-ticker.php
+++ rss-cronkite-ticker.php
@@ -5,7 +5,7 @@
 define('DONOTCACHEPAGE', true);
 header('Content-Type: '.feed_content_type('rss2').';charset='.get_option('blog_charset'), true);
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
-do_action( 'rss_tag_pre', 'rss2' );
+do_action('rss_tag_pre', 'rss2');
 ?>
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -22,13 +22,13 @@ do_action( 'rss_tag_pre', 'rss2' );
 	<?php do_action('rss2_head'); ?>
 
 <?php $separator = "\r\n";
-    $line = strtok($body, $separator);
-    while ($line !== false):
-    $line = sanitize_text_field( $line );?>
+$line = strtok($body, $separator);
+while ($line !== false):
+    $line = sanitize_text_field($line);?>
 	<item>
 		<description><![CDATA[<?php echo wp_filter_nohtml_kses($line) ?>]]></description>
-	<?php do_action( 'rss2_item' ); ?></item>
-<?php $line = strtok( $separator ); ?>
+	<?php do_action('rss2_item'); ?></item>
+<?php $line = strtok($separator); ?>
 <?php endwhile; ?>
 </channel>
 </rss>
